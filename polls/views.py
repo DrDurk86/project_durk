ce48c343f7deddd79516f0beaea13b6e71e038eb
drdurk86 <drdurk86@gmail.com>
2021-03-06 01:34:45 +0300

    Обновил библиотеки во views

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,7 @@
-from django.template import loader
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-
-
+from django.urls import reverse
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
